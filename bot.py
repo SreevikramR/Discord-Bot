aa6ca373fd59410641542f978304997af9d3af99
SreevikramR <48626706+SreevikramR@users.noreply.github.com>
2021-03-12 20:21:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands, tasks
 import random
-from itertools import cycle
+from PIL import Image, ImageFilter
+from io import BytesIO
 
 client = commands.Bot(command_prefix=".")
 
@@ -24,6 +25,14 @@ async def ping(ctx):
     )
     embed.set_footer(text="Bot made by TheRealViki#3300")
     await ctx.send(embed=embed)
+
+
+@client.command()
+async def servers(ctx):
+    await ctx.author.send("**The servers bot is connected to are below -**")
+    if ctx.author.display_name == "TheRealViki":
+        for guild in client.guilds:
+            await ctx.author.send(f'{guild.name}')
 
 
 @client.command()
@@ -75,7 +84,7 @@ async def clear(ctx, amount : int):
 @clear.error
 async def clear_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Please specify an amount of messages to delete in the form of   **.clear <number>**')
+        await ctx.send('Please specify an amount of messages to delete in the form of   **.clear number**')
 
 
 @client.group(invoke_without_command=True)
@@ -88,12 +97,23 @@ async def help(ctx, *, message=None):
         )
 
         embed.add_field(name="These commands require admin permissions to run", value="**  **", inline=False)
-        embed.add_field(name='__.warn <member> <reason__>', value='Warns a member for any specifiable reason through DM', inline=False)
-        embed.add_field(name='__.kick <member>__', value='Kicks a member from the server', inline=False)
-        embed.add_field(name='__.ban <member>__', value='Bans a member from the server', inline=False)
-        embed.add_field(name='__.unban <member#1243>__', value='Unbans a member in the server', inline=False)
+        embed.add_field(name='__.warn @member reason__', value='Warns a member for any specifiable reason through DM', inline=False)
+        embed.add_field(name='__.kick @member__', value='Kicks a member from the server', inline=False)
+        embed.add_field(name='__.ban @member__', value='Bans a member from the server', inline=False)
+        embed.add_field(name='__.unban @member#1243__', value='Unbans a member in the server', inline=False)
         embed.set_footer(text="Bot made by TheRealViki#3300")
         msg = await ctx.send(embed=embed)
+        return
+
+    elif message == "canvas":
+        embed = discord.Embed(
+            title='**Canvas Commands**',
+            decription='These commands edit pictures and send them',
+            colour=discord.Colour.blue()
+        )
+        embed.add_field(name='__.wanted @member__', value='Shows the person in a wanted poster', inline=False)
+        embed.add_field(name='__.trash @member__', value='The best way to trash talk a friend', inline=False)
+        await ctx.send(embed=embed)
         return
 
 
@@ -102,12 +122,13 @@ async def help(ctx, *, message=None):
         colour = discord.Colour.blue()
     )
     embed.add_field(name='__.help mod__', value='View all moderation commands (Admin Permissions required)',inline=False)
-    embed.add_field(name='__.ask <question>__', value='Ask any question that will give you a yes or no answer', inline=False)
-    embed.add_field(name='__.clear <number>__', value='Clears <number> of messages in the text channel', inline=False)
+    embed.add_field(name='__.help canvas__', value='View all picture editing related commands',inline=False)
+    embed.add_field(name='__.ask question__', value='Ask any question that will give you a yes or no answer', inline=False)
+    embed.add_field(name='__.clear number__', value='Clears <number> of messages in the text channel', inline=False)
     embed.add_field(name='__.ping__', value='Gives you the Ping of the bot', inline=False)
-    embed.add_field(name='__.tell <member> <message>__', value='DM anyone by mentioning them with the message through the bot', inline=False)
-    embed.add_field(name="__.embed <text>__", value="Embeds the text.")
-    embed.add_field(name='Octave Support Server', value ='[Support Server](https://discord.com/api/oauth2/authorize?client_id=816985180459892746&permissions=8&scope=bot)', inline=False)
+    embed.add_field(name='__.tell member message__', value='DM anyone by mentioning them with the message through the bot', inline=False)
+    embed.add_field(name="__.embed text__", value="Embeds the text.")
+    embed.add_field(name='Octave Support Server', value ='[Support Server](https://discord.gg/dKhHVAXxQx)', inline=False)
     embed.set_footer(text="Bot made by TheRealViki#3300")
     msg = await ctx.send(embed=embed)
 
@@ -123,7 +144,7 @@ async def embed(ctx, *, message):
 @embed.error
 async def embed_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Please specify what you want to embed in the form of **.embed <text>**')
+        await ctx.send('Please specify what you want to embed in the form of **.embed text**')
 
 
 @client.command()
@@ -139,6 +160,8 @@ async def kick(ctx, member: discord.Member, *, reason=None):
 async def kick_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send('You do not have admin permissions to run this command')
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please specify who you would like to kick in the form of **.kick @member**')
 
 
 @client.command()
@@ -161,6 +184,8 @@ async def ban_error(ctx, error):
 async def ben_error(ctx, error):
     if isinstance(error, commands.MemberNotFound):
         await ctx.send('Member was not found')
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please specify who you would like to ban in the form of **.ban @member**')
 
 
 @client.command()
@@ -190,7 +215,7 @@ async def tell(ctx, member: discord.Member, *, message):
 @tell.error
 async def tell_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Please specify who you want to dm to by mentioning them and the message in the form of **.embed <member> <message>**')
+        await ctx.send('Please specify who you want to dm to by mentioning them and the message in the form of **.embed @member message**')
 
 
 @client.command()
@@ -204,7 +229,68 @@ async def warn(ctx, member: discord.Member, *, reason):
 @warn.error
 async def tell_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Please specify who you want to warn by mentioning them and the reason of warning **.warn <member> <reason>**')
+        await ctx.send('Please specify who you want to warn by mentioning them and the reason of warning **.warn @member reason**')
+
+
+@client.command()
+async def wanted(ctx, user: discord.Member):
+    await ctx.channel.purge(limit=1)
+    await ctx.send("Generating...")
+    wanted = Image.open("wanted-poster.jpg")
+
+    asset = user.avatar_url_as(size=128)
+    data = BytesIO(await asset.read())
+    pfp = Image.open(data)
+    pfp = pfp.resize((240, 310))
+    wanted.paste(pfp, (193, 260))
+
+    wanted.save("profile.jpg")
+
+    await ctx.channel.purge(limit=1)
+    await ctx.send(file = discord.File("profile.jpg"))
+
+
+@wanted.error
+async def wanted_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please specify who you would like to include in the picture in the form of **.wanted member**')
+
+
+@client.command()
+async def trash(ctx, user: discord.Member):
+    await ctx.channel.purge(limit=1)
+    await ctx.send("Generating...")
+
+    trash = Image.open("trash-meme.png")
+
+    asset = user.avatar_url_as(size=128)
+    data = BytesIO(await asset.read())
+    pfp = Image.open(data)
+    pfp = pfp.filter(ImageFilter.BLUR)
+    pfp = pfp.resize((315, 310))
+    trash.paste(pfp, (306, 0))
+    trash.save("trash.png")
+
+    await ctx.channel.purge(limit=1)
+    await ctx.send(file=discord.File("trash.png"))
+
+
+@trash.error
+async def trash_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please specify who you would like to include in the picture in the form of **.trash member**')
+
+
+@client.command()
+async def say(ctx, *, message):
+    await ctx.channel.purge(limit=1)
+    await ctx.send(message)
+
+
+@say.error
+async def say_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please specify what you would like the bot to say in the form of **.say text**')
 
 
 client.run("ODE2OTg1MTgwNDU5ODkyNzQ2.YEC6vQ.LTlfnV8tlxgZeUd_Tar465HBymo")
